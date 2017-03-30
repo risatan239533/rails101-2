eeac182b42973d77a4ae1eef8e0e14f7e7a2a21e
RISA <risa@RISAdeMacBook-Pro.local>
2017-03-31 05:16:39 +0800

    implement groups#destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -30,7 +30,12 @@ def update
   redirect_to groups_path, notice: "Update Success"
 end
 
-private
+def destroy
+  @group = Group.find(params[:id])
+  @group.destroy
+  flash[:alert] = "Group deleted"
+  redirect_to groups_path
+end
 
 def group_params
   params.require(:group).permit(:title, :description)
